8c0171076619381043006832f05c2e6b784acf47
kiabq <conway.joseph@pm.me>
2023-06-20 18:32:05 -0400

    add: middleware and db packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    root: './client',
-    srcDir: './client/src'
+    srcDir: 'client/src'
 });
