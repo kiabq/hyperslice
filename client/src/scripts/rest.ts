98678501667635aa2b37ca9ae948cc322f899dcf
kiabq <conway.joseph@pm.me>
2023-06-22 03:12:35 -0400

    add: checkHTTP function
    - this function uses a basic regexp to check if the link begins with https or http

--- client/src/scripts/rest.ts
+++ client/src/scripts/rest.ts
@@ -11,14 +11,26 @@ async function post(url: string, data={}) {
     return response;
 }
 
+function checkHTTP(url: string) {
+    const beginsWith = new RegExp(/^(http|https):\/\//);
+
+    if (!beginsWith.test(url)) {
+        return 'http://' + url;
+    } else {
+        return url;
+    }
+}
+
 export default (function() {
     const form = document.querySelector("#form") as HTMLFormElement;
     const input = document.querySelector("#link-input") as HTMLInputElement;
-    
+
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        const url = encodeURIComponent(input.value);
-        post("http://localhost:3001", { data: url })
+
+        const url = checkHTTP(input.value);
+        const encodedURL = encodeURIComponent(url);
+        post("http://localhost:3001", { data: encodedURL })
         .then((response) => {
             if (response.status == 200) {
                 form.reset();
