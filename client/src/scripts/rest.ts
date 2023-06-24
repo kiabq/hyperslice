8593a980b4f18d69b2110dbe3ddd730a14d323a9
kiabq <conway.joseph@pm.me>
2023-06-24 19:00:15 -0400

    feat: client-side error handling and validation for inputs

--- client/src/scripts/rest.ts
+++ client/src/scripts/rest.ts
@@ -1,3 +1,4 @@
+
 async function post(url: string, data={}) {    
     const response = await fetch(url, {
         method: "POST",
@@ -6,6 +7,8 @@ async function post(url: string, data={}) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
+    }).then((response) => {
+        return response.json();
     })
 
     return response;
@@ -22,19 +25,53 @@ function checkHTTP(url: string) {
 }
 
 export default (function() {
-    const form = document.querySelector("#form") as HTMLFormElement;
+    const form = document.querySelector("#form-long") as HTMLFormElement;
     const input = document.querySelector("#link-input") as HTMLInputElement;
+    const inputError = document.querySelector('.link-error') as HTMLSpanElement;
+    const shortened = document.querySelector("#link-short") as HTMLInputElement;
+    const banned = import.meta.env.PUBLIC_APEX;
+    const bannedRegex = new RegExp(banned, "i");
+
+    // What in the everloving fuck is this??
+    input.addEventListener("invalid", (e) => {
+        e.preventDefault();
+        inputError.innerText = input.validationMessage.toLowerCase();
+        input.setCustomValidity("");
+        input.title = "";
+        input.classList.add("link-invalid");
+    })
+
+    input.addEventListener("input", (e) => {
+        const element = (e.target as HTMLInputElement);
+        input.setCustomValidity("");
+
+        if (bannedRegex.test(element.value)) {
+            input.setCustomValidity('Banned URL');
+            input.classList.add('link-invalid');
+            inputError.innerText = input.validationMessage.toLowerCase();
+        } else if (!input.validity.valid) {
+            input.classList.add('link-invalid');
+            inputError.innerText = input.validationMessage.toLowerCase();
+        } else {
+            input.classList.remove('link-invalid');
+        }
+    })
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
         const url = checkHTTP(input.value);
         const encodedURL = encodeURIComponent(url);
-        post("http://localhost:3001", { data: encodedURL })
-        .then((response) => {
-            if (response.status == 200) {
-                form.reset();
-            }
-        })
+        
+        // Throw error for form to pick up
+        if (bannedRegex.test(input.value)) {
+            input.setCustomValidity('Banned URL');
+        } else {
+            post("http://localhost:3001/", { data: encodedURL })
+                .then((response) => {
+                    form.reset();
+                    shortened.value = response.data;
+                })
+        }
     })
 })();
