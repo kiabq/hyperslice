25cc7356c77fe99b23e4730c74c9c548779e7247
kiabq <conway.joseph@pm.me>
2023-06-26 02:37:19 -0400

    add: vercel adapter to astro config

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
+    adapter: vercelStatic(),
     experimental: {
         assets: true
     }
