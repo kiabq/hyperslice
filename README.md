b70489e9b034039e9d3a00db8aaf39999f0809f7
kiabq <conway.joseph@pm.me>
2023-06-26 09:46:42 -0400

    add: wave background

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# short-link
+# HyperSlice
+## a simple link shortener.
+
+##  TODO
+- Make a better README
+- Document Code
--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
     experimental: {
         assets: true
     }
