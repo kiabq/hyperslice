0a2b1795aa0eba8d62df20c98c63d8dac0b4aad4
kiabq <conway.joseph@pm.me>
2023-06-26 06:14:13 -0400

    update: migrating

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: vercel(),
     experimental: {
         assets: true
     }
