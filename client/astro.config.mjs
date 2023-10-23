099e2da84fe56d70651129db693d206689bf0ad7
kiabq <conway.joseph@pm.me>
2023-10-23 14:31:40 -0400

    fix: import

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
     experimental: {
         assets: true
     },
+    output: "server",
     adapter: vercel({
         webAnalytics: {
             enabled: true
