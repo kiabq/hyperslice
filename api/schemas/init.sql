fc57961b7abb55ad2d4f84203e3613efbdac7042
kiabq <conway.joseph@pm.me>
2024-04-13 22:55:26 -0400

    chore: move schemas to init file

--- api/schemas/init.sql
+++ api/schemas/init.sql
@@ -1,3 +1,10 @@
+-- Create 'links' table --
+CREATE TABLE links (
+	id serial4 NOT NULL,
+	link varchar(2048) NOT NULL DEFAULT '',
+	CONSTRAINT links_pkey PRIMARY KEY (id)
+);
+
 -- Create alias table --
 -- id is set to a serial so it doesn't need to be manually incremented --
 CREATE TABLE alias (
--- api/schemas/links.sql
+++
@@ -1,6 +0,0 @@
--- Create 'links' table --
-CREATE TABLE links (
-	id serial4 NOT NULL,
-	link varchar(2048) NOT NULL DEFAULT '',
-	CONSTRAINT links_pkey PRIMARY KEY (id)
-);
